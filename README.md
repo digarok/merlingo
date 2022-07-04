840bd4f364bf4cd54006c07e4c04c257d5c16b66
Dagen Brock <dagenbrock@gmail.com>
2022-07-03 19:35:10 -0500

    look ma, I wrote a(nother) assembly formatter :P

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # merlingo
 Formatter library in golang for Merlin Assembly Language syntax
+
+## Current Use
+This is a part of Appy, which can assemble and package your Apple II/IIgs projects written in Merlin (8/16/32).
+
--- merlingo.go
+++ merlingo.go
@@ -2,33 +2,216 @@ package merlingo
 
 import (
 	"bufio"
-	"fmt"
+	"errors"
+	"log"
 	"os"
+	"strings"
+	"unicode"
 )
 
-// Hello returns a greeting for the named person.
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
-}
+//  params/defaults
+var mnemonic_col_x = 18
+var operand_col_x = 24
+var comment_col_x = 48
+var min_space = 1
+var bump_space = 2
+var indent_semi = true
+var indent_ast = false
 
-func Fmt(code string) string {
-	return code
+func fmtLine(code string) (string, error) {
+
+	// state machine - resets each line
+	in_quote := false
+	in_comment := false
+	label_done := false
+	in_label := false
+	opcode_done := false
+	in_opcode := false
+	operand_done := false
+	in_operand := false
+	var quote_char string
+	x := 0
+
+	buf := "" // line buffer, starts empty each line and is appended to output_buf
+	label_done = true
+
+	// scan by char
+	for i, c := range code {
+		// starts with whitespace? do an indent
+		if i == 0 && unicode.IsSpace(c) {
+			buf += strings.Repeat(" ", mnemonic_col_x)
+			x += mnemonic_col_x
+			label_done = true
+			continue // SHORT CIRCUIT
+		}
+
+		// are we in a comment? just print the char
+		if in_comment {
+			buf += string(c)
+			x += 1
+			continue // SHORT CIRCUIT
+		}
+
+		// are we in a quote? print, but also look for matching end quote
+		if in_quote {
+			buf += string(c)
+			x += 1
+			if string(c) == quote_char { // did we find closing quotes?
+				in_quote = false
+			}
+			continue // SHORT CIRCUIT
+		}
+
+		// not already in comment or quote
+		if unicode.IsSpace(c) {
+			// ignore
+			if in_label {
+				in_label = false
+				label_done = true
+				// do we need to bump out space ?
+				if x > mnemonic_col_x-min_space {
+					buf += strings.Repeat(" ", min_space)
+					x += min_space
+				} else {
+					buf += strings.Repeat(" ", mnemonic_col_x-x)
+					x += mnemonic_col_x - x
+				}
+			} else if in_opcode {
+				in_opcode = false
+				opcode_done = true
+				// do we need to bump out space ?
+				if x > operand_col_x-min_space {
+					buf += strings.Repeat(" ", min_space)
+					x += min_space
+				} else {
+					buf += strings.Repeat(" ", operand_col_x-x)
+					x += operand_col_x - x
+				}
+			} else if in_operand {
+				in_operand = false
+				operand_done = true
+				// do we need to bump out space ?
+				if x > comment_col_x-min_space {
+					buf += strings.Repeat(" ", min_space)
+					x += min_space
+				} else {
+					buf += strings.Repeat(" ", comment_col_x-x)
+					x += comment_col_x - x
+				}
+			}
+
+			continue
+		} else {
+			// see if we are starting a quote
+			if string(c) == "\"" || string(c) == "'" {
+				quote_char = string(c)
+				in_quote = true
+				in_operand = true
+				buf += string(c)
+
+				// see if we are starting a line with a comment
+			} else if (c == ';' || c == '*') && i == 0 {
+				in_comment = true
+				buf += string(c)
+				x += 1
+
+				// found a semi-colon not in an operand (macro!danger)
+				//  (and not in quote or comment)
+			} else if c == ';' && !in_operand {
+				in_comment = true
+				// protect against "negative" spacing
+				var spaces = 0
+				if 0 <= (comment_col_x - x) {
+					spaces = comment_col_x - x
+				}
+				buf += strings.Repeat(" ", spaces)
+
+				x += comment_col_x - x
+				buf += string(c)
+				x += 1
+
+				// found asterisk preceded only by whitespace
+			} else if c == '*' && strings.Replace(string(code[0:i-1]), " ", "", -1) == "" {
+				in_comment = true
+				buf += string(c)
+				x += 1
+
+				// real label!
+			} else if i == 0 {
+				in_label = true
+				buf += string(c)
+				x += 1
+
+				// already in label?
+			} else if in_label {
+				buf += string(c)
+				x += 1
+
+				// real opcode!
+			} else if label_done && !opcode_done {
+				in_opcode = true
+				buf += string(c)
+				x += 1
+
+				// already in opcode
+			} else if in_opcode {
+				buf += string(c)
+				x += 1
+
+				// real operand!
+			} else if opcode_done && !operand_done {
+				in_operand = true
+				buf += string(c)
+				x += 1
+
+				// already in operand
+			} else if in_operand {
+				buf += string(c)
+				x += 1
+
+				// if they have unhandled weirdness, just pass them through minus whitespace
+			} else {
+				if !unicode.IsSpace(c) {
+					buf += string(c)
+					x += 1
+				}
+			}
+		}
+
+	}
+
+	// check for lossiness
+	c1 := strings.Replace(string(code), " ", "", -1)
+	c2 := strings.Replace(buf, " ", "", -1)
+	if c1 != c2 {
+		return buf, errors.New("error on line")
+	}
+
+	// this adds a newline, not sure about cross-platformness
+	return buf + "\n", nil
 }
 
 func FmtFile(filename string) {
-	fmt.Println("Formatting ", filename)
 	readFile, err := os.Open(filename)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
 
+	buf := ""
+	i := 0
 	for fileScanner.Scan() {
-		fmt.Println(fileScanner.Text())
+		original_line := fileScanner.Text()
+		formatted_line, err := fmtLine(original_line)
+		if err != nil {
+			log.Fatal(err, i)
+		}
+		buf += formatted_line
+		i += 1
 	}
-
 	readFile.Close()
-
+	if err := os.WriteFile(filename, []byte(buf), 0666); err != nil {
+		log.Fatal(err)
+	}
 }
